92c74d41313caaf4a21af73919e1c19c9ee79878
jihaifeng <1104128141@qq.com>
2017-08-23 13:18:25 +0800

    q

--- src/main/java/TestWeChat.java
+++ src/main/java/TestWeChat.java
@@ -1,7 +1,11 @@
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -12,6 +16,7 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,56 +28,44 @@ import java.util.concurrent.TimeUnit;
  */
 public class TestWeChat {
     private AndroidDriver driver;
-    private boolean isInstall = false;
-
-    public void startRecord() throws IOException {
-        Runtime rt = Runtime.getRuntime();
-        // this code for record the screen of your device
-        rt.exec("cmd.exe /C adb shell screenrecord /sdcard/runCase.mp4");
-    }
-
-    @BeforeClass(alwaysRun = true)
+    @Before
     public void setUp() throws Exception {
-        //启动appium
+        //设置apk的路径
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "apps");
+        File app = new File(appDir, "ContactManager.apk");
+
+        //设置自动化相关参数
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("deviceName", "lge-nexus_4-005475cbccd279d4");
-        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "4.4.2");
+        capabilities.setCapability("deviceName", "Android Emulator");
 
-        //配置测试apk
-        capabilities.setCapability("appPackage", "com.zhihu.android");
-        capabilities.setCapability("appActivity", ".app.ui.activity.MainActivity ");
-        capabilities.setCapability("sessionOverride", true);    //每次启动时覆盖session，否则第二次后运行会报错不能新建session
-        capabilities.setCapability("unicodeKeyboard", true);    //设置键盘
-        capabilities.setCapability("resetKeyboard", false);     //设置默认键盘为appium的键盘
-        URL url = new URL("http://127.0.0.1:4723/wd/hub");
-        driver = new AndroidDriver(url, capabilities);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //设置安卓系统版本
+        capabilities.setCapability("platformVersion", "4.3");
+        //设置apk路径
+        capabilities.setCapability("app", app.getAbsolutePath());
 
-        //如果真机设备已经安装知乎，则不需要重新安装
-        if (isInstall) {
-            File classpathRoot = new File(System.getProperty("user.dir"));
-            File appDir = new File(classpathRoot, "apps");
-            File app = new File(appDir, "zhihu.apk");
-            capabilities.setCapability("app", app.getAbsolutePath());
-        }
-        startRecord();
+        //设置app的主包名和主类名
+        capabilities.setCapability("appPackage", "com.example.android.contactmanager");
+        capabilities.setCapability("appActivity", ".ContactManager");
+
+        //初始化
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
     @Test
-    public void loginWithMicroBlog() throws InterruptedException {
-        // swipe to right
-        driver.findElementById("com.zhihu.android:id/register_btn").click();
-        driver.findElementById("com.zhihu.android:id/username").sendKeys("手机号");
-        driver.findElementById("com.zhihu.android:id/password").sendKeys("登录密码");
-        driver.findElementById("com.zhihu.android:id/fullname").sendKeys("用户昵称");
-        WebElement NxtBtn = driver.findElementById("com.zhihu.android:id/btn_progress");
-        Assert.assertEquals(NxtBtn.isEnabled(), true);
-        Thread.sleep(2000);
+    public void addContact(){
+        WebElement el = driver.findElement(By.name("Add Contact"));
+        el.click();
+        List<WebElement> textFieldsList = driver.findElementsByClassName("android.widget.EditText");
+        textFieldsList.get(0).sendKeys("Some Name");
+        textFieldsList.get(2).sendKeys("Some@example.com");
+        driver.swipe(100, 500, 100, 100, 2);
+        driver.findElementByName("Save").click();
     }
 
-    @AfterClass(alwaysRun = true)
+    @After
     public void tearDown() throws Exception {
         driver.quit();
     }
